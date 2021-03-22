dab0ab11b1c71c68ccdb4622194a47bda6778ab3
Ava <iamsimplesthing@gmail.com>
2021-03-22 15:14:51 -0700

    directional light

--- src/client/client.ts
+++ src/client/client.ts
@@ -8,8 +8,11 @@ const scene: THREE.Scene = new THREE.Scene()
 const axesHelper = new THREE.AxesHelper(5)
 scene.add(axesHelper)
 
-var light = new THREE.AmbientLight();
+var light = new THREE.DirectionalLight();
 scene.add(light);
+
+var helper = new THREE.DirectionalLightHelper(light);
+scene.add(helper);
 
 const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 
@@ -62,6 +65,10 @@ torus[2].position.x = 0
 torus[3].position.x = 4
 torus[4].position.x = 8
 
+light.target = torus[0]
+light.target.position.set(0, 10, 0)
+scene.add(light.target)
+
 scene.add(torus[0])
 scene.add(torus[1])
 scene.add(torus[2])
@@ -90,8 +97,11 @@ const lightFolder = gui.addFolder('THREE.Light')
 lightFolder.addColor(data, 'color').onChange(() => { light.color.setHex(Number(data.color.toString().replace('#', '0x'))) });
 lightFolder.add(light, 'intensity', 0, 1, 0.01);
 
-const ambientLightFolder = gui.addFolder('THREE.AmbientLight')
-ambientLightFolder.open()
+const directionalLightFolder = gui.addFolder('THREE.DirectionalLight')
+directionalLightFolder.add(light.position, "x", -100, 100, 0.01)
+directionalLightFolder.add(light.position, "y", -100, 100, 0.01)
+directionalLightFolder.add(light.position, "z", -100, 100, 0.01)
+directionalLightFolder.open()
 
 const meshesFolder = gui.addFolder('Meshes')
 meshesFolder.add(data, 'mapsEnabled').onChange(() => {
@@ -107,6 +117,8 @@ meshesFolder.add(data, 'mapsEnabled').onChange(() => {
 
 var animate = function () {
     requestAnimationFrame(animate)
+
+    helper.update()
 
     torus.forEach(t => {
         t.rotation.y += .01
